acf5d5e5ea4d4b660f207806cbed3dde96333416
Zech Zimmerman <hi@zech.codes>
2022-08-14 21:58:55 -0400

    Make the state machine more human readable

--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -19,7 +19,7 @@ async def two_state_machine():
         async def state_b(self, value):
             self.result.append("state_b_run")
 
-        @state_a >> state_b
+        @state_a.goes_to(state_b).when
         async def transition_a_to_b(self, value):
             if value == "b":
                 self.result.append("a_to_b")
@@ -27,7 +27,7 @@ async def two_state_machine():
 
             return False
 
-        @state_b >> state_a
+        @state_b.goes_to(state_a).when
         async def transition_b_to_a(self, value):
             if value == "a":
                 self.result.append("b_to_a")
@@ -36,24 +36,6 @@ async def two_state_machine():
             return False
 
     return await Machine().start(Machine.state_a, "")
-
-
-@pytest_asyncio.fixture()
-async def two_state_failure_machine():
-    class Machine(StateMachine):
-        @State
-        async def fail_a(self, value):
-            ...
-
-        @State
-        async def fail_b(self, value):
-            ...
-
-        @fail_a >> fail_b
-        async def transition_a_to_b(self, value):
-            raise Exception()
-
-    return await Machine().start(Machine.fail_a, "")
 
 
 @pytest.mark.asyncio
@@ -73,3 +55,100 @@ async def test_state_machine(two_state_machine):
 async def test_unsuccessful_state_no_transition(two_state_machine):
     with pytest.raises(WordletteNoTransitionFound):
         await two_state_machine.next("a")
+
+
+@pytest.mark.asyncio
+async def test_aggregate_transitions():
+    states = []
+
+    class TestMachine(StateMachine):
+        @State
+        async def A(self, arg):
+            states.append("A")
+
+        @State
+        async def B(self, arg):
+            states.append("B")
+
+        @State
+        async def C(self, arg):
+            states.append("C")
+
+        @ (A & B).goes_to(C).when
+        async def arg_is_c(self, arg):
+            return arg == "C"
+
+        C.goes_to(B)
+
+    machine = TestMachine()
+    await machine.start(TestMachine.A, "STARTING")
+    await machine.next("C")
+    await machine.next("B")
+    await machine.next("C")
+    assert states == ["A", "C", "B", "C"]
+
+
+@pytest.mark.asyncio
+async def test_aggregate_transitions():
+    states = []
+
+    class TestMachine(StateMachine):
+        @State
+        async def A(self, arg):
+            states.append("A")
+
+        @State
+        async def B(self, arg):
+            states.append("B")
+
+        @State
+        async def C(self, arg):
+            states.append("C")
+
+        @ (A & B).goes_to(C).when
+        async def arg_is_c(self, arg):
+            return arg == "C"
+
+        C.goes_to(B)
+
+    machine = TestMachine()
+    await machine.start(TestMachine.A, "STARTING")
+    await machine.next("C")
+    await machine.next("B")
+    await machine.next("C")
+    assert states == ["A", "C", "B", "C"]
+
+
+@pytest.mark.asyncio
+async def test_default_transitions():
+    states = []
+
+    class TestMachine(StateMachine):
+        @State
+        async def A(self, arg):
+            states.append("A")
+
+        @State
+        async def B(self, arg):
+            states.append("B")
+
+        @State
+        async def C(self, arg):
+            states.append("C")
+
+        @ (A & B).goes_to(C).when
+        async def arg_is_c(self, arg):
+            return arg == "C"
+
+        C.goes_to(B)
+        A.goes_to(B)
+        B.goes_to(A)
+
+    machine = TestMachine()
+    await machine.start(TestMachine.A, "STARTING")
+    await machine.next("GO TO B")  # At A, will go to B
+    await machine.next("GO TO A")  # At B, will go to A
+    await machine.next("C")  # At A, will go to C
+    await machine.next("GO TO B")  # At C, will go to B
+    await machine.next("C")  # At B, will go to C
+    assert states == ["A", "B", "A", "C", "B", "C"]
--- wordlette/state_machine/state.py
+++ wordlette/state_machine/state.py
@@ -18,18 +18,18 @@ class State(Generic[T, P]):
         self.transitions: dict[State[T, P], Transition] = {}
         self.name = None
 
+    def __and__(self, other) -> AggregateState:
+        return AggregateState(self, other)
+
     def __repr__(self):
         return f"{type(self).__name__}({self.name!r})"
 
-    def __rshift__(self, to_state: State) -> Callable[[Predicate], Transition]:
-        def get_predicate(predicate: Predicate) -> Transition:
-            self.transitions[to_state] = Transition(self, to_state, predicate)
-            return self.transitions[to_state]
-
-        return get_predicate
-
     def __set_name__(self, owner, name):
         self.name = name
+
+    def goes_to(self, to_state: State) -> Transition:
+        self.transitions[to_state] = Transition(self, to_state)
+        return self.transitions[to_state]
 
     async def leave(self, *args: P.args, **kwargs: P.args):
         if self._leave:
@@ -53,14 +53,52 @@ class State(Generic[T, P]):
 
 
 class Transition:
-    def __init__(self, from_state: State, to_state: State, predicate: Predicate):
+    def __init__(
+        self, from_state: State, to_state: State, predicate: Predicate | None = None
+    ):
         self.from_state = from_state
         self.to_state = to_state
-        self.predicate = predicate
         self.name = None
+        self.predicate = predicate or self._predicate
 
     def __set_name__(self, owner, name):
         self.name = name
 
     async def can_transition(self, *args: P.args, **kwargs: P.kwargs) -> bool:
         return await self.predicate(*args, **kwargs)
+
+    def when(self, predicate: Predicate) -> Transition:
+        self.predicate = predicate
+        return self
+
+    async def _predicate(self, *_, **__) -> bool:
+        return True
+
+
+class AggregateState:
+    def __init__(self, *states):
+        self.states = list(states)
+
+    def __and__(self, other):
+        self.states.append(other)
+        return self
+
+    def goes_to(self, to_state: State) -> SharedTransition:
+        return SharedTransition(*self.states, to_state=to_state)
+
+
+class SharedTransition:
+    def __init__(self, *from_states: State, to_state: State):
+        self.from_states = from_states
+        self.to_state = to_state
+        self.transitions = [state.goes_to(self.to_state) for state in from_states]
+
+    def when(self, predicate: Predicate) -> SharedTransition:
+        self.transitions = [
+            transition.when(predicate) for transition in self.transitions
+        ]
+
+        return self
+
+    def __set_name__(self, owner, name):
+        self.name = name
