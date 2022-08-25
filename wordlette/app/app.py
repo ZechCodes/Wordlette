a3a41de6f38f4f8005c88c9c36509ff8441e39de
Zech Zimmerman <hi@zech.codes>
2022-08-25 18:37:06 -0400

    Log state changes

--- wordlette/app/app.py
+++ wordlette/app/app.py
@@ -1,7 +1,7 @@
 from wordlette.app.app_protocol import AppProtocol
 from wordlette.state_machine import StateMachine
-from wordlette.app.app_state import AppState
-from bevy import Bevy, Context
+from wordlette.app.app_state import AppState, StateChangeEvent
+from bevy import Bevy, Context, Inject
 from wordlette.smart_functions import call
 from typing import Callable, Iterator, Type, TypeAlias
 from starlette.types import Receive, Scope, Send
@@ -9,6 +9,8 @@ from starlette.applications import Starlette
 from wordlette.exceptions import WordletteNoStarletteAppFound
 import logging
 import uvicorn
+from wordlette.events import EventManager
+from copy import deepcopy
 
 
 StateMachineConstructor: TypeAlias = (
@@ -17,14 +19,21 @@ StateMachineConstructor: TypeAlias = (
     | Callable[[], StateMachine]
 )
 
-logger = logging.root.getChild("APP")
+logger = logging.getLogger("wordlette")
 
 
 class App(Bevy):
+    events: EventManager = Inject
+
     def __init__(self, state_machine_constructor: StateMachineConstructor = AppState):
-        self._state = call(state_machine_constructor, self)
+        self.events.listen({"type": "changing-state"}, self._log_state_transition)
+        self._state = call(self.bevy.bind(state_machine_constructor), self)
+
+    async def _log_state_transition(self, event: StateChangeEvent):
+        logger.info(f"{event.old_state} -> {event.new_state}")
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send):
+        logging.getLogger("uvicorn.error").setLevel(logging.ERROR)
         if not self.state.started:
             await self._start()
 
@@ -61,4 +70,31 @@ class App(Bevy):
     @classmethod
     def start(cls, host: str, port: int, extensions_modules: Iterator[str]):
         app = cls()
-        uvicorn.run(app, host=host, port=port)
+        uvicorn.run(app, host=host, port=port, log_config=cls._create_logging_config())
+
+    @staticmethod
+    def _create_logging_config():
+        log_config = deepcopy(uvicorn.config.LOGGING_CONFIG)
+
+        log_config["formatters"]["default"][
+            "fmt"
+        ] = f'  UVICORN.{log_config["formatters"]["default"]["fmt"]}'
+        log_config["formatters"]["access"][
+            "fmt"
+        ] = f'   ACCESS.{log_config["formatters"]["access"]["fmt"]}'
+
+        log_config["formatters"]["wordlette"] = {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "WORDLETTE.%(levelprefix)s %(message)s",
+            "use_colors": None,
+        }
+        log_config["handlers"]["wordlette"] = {
+            "formatter": "wordlette",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        }
+        log_config["loggers"]["wordlette"] = {
+            "handlers": ["wordlette"],
+            "level": "INFO",
+        }
+        return log_config
