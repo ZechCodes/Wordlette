3f46420a45324b32c17dd3bb12b18fe8528caec0
Zech Zimmerman <hi@zech.codes>
2022-08-28 11:51:01 -0400

    Create a simple extension loader that can scan for classes

---
+++ wordlette/extensions/loader.py
@@ -0,0 +1,33 @@
+from importlib import import_module
+from typing import Iterable, TypeVar
+from types import ModuleType
+
+
+T = TypeVar("T", bound=type)
+
+
+def load_extension_package(
+    package_import_path: str,
+    scan_for: Iterable[T] = tuple(),
+    package: str | None = None,
+) -> tuple[ModuleType, dict[str, set[T]]]:
+    return (
+        import_module(package_import_path, package),
+        _get_all_subclasses_in_package(scan_for, package_import_path),
+    )
+
+
+def _get_all_subclasses_in_package(
+    scan_for: Iterable[T], search_package: str
+) -> dict[str, set[T]]:
+    return {
+        scan_type: set(_get_subclasses_in_package(scan_type, search_package))
+        for scan_type in scan_for
+    }
+
+
+def _get_subclasses_in_package(base_class: T, search_package: str) -> set[T]:
+    for subclass in base_class.__subclasses__():
+        if subclass.__module__.startswith(search_package):
+            yield subclass
+            yield from _get_subclasses_in_package(subclass, search_package)
