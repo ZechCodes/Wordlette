8a99e0a419c79b47bf7a7b93a840915022600e62
Zech Zimmerman <hi@zech.codes>
2022-08-28 15:57:38 -0400

    Correct extension loader type hints

--- wordlette/extensions/loader.py
+++ wordlette/extensions/loader.py
@@ -1,5 +1,5 @@
 from importlib import import_module
-from typing import Iterable, TypeVar
+from typing import Generator, Iterable, TypeVar
 from types import ModuleType
 
 
@@ -10,7 +10,7 @@ def load_extension_package(
     package_import_path: str,
     scan_for: Iterable[T] = tuple(),
     package: str | None = None,
-) -> tuple[ModuleType, dict[str, set[T]]]:
+) -> tuple[ModuleType, dict[T, set[T]]]:
     return (
         import_module(package_import_path, package),
         _get_all_subclasses_in_package(scan_for, package_import_path),
@@ -19,14 +19,16 @@ def load_extension_package(
 
 def _get_all_subclasses_in_package(
     scan_for: Iterable[T], search_package: str
-) -> dict[str, set[T]]:
+) -> dict[T, set[T]]:
     return {
         scan_type: set(_get_subclasses_in_package(scan_type, search_package))
         for scan_type in scan_for
     }
 
 
-def _get_subclasses_in_package(base_class: T, search_package: str) -> set[T]:
+def _get_subclasses_in_package(
+    base_class: T, search_package: str
+) -> Generator[None, T, None]:
     for subclass in base_class.__subclasses__():
         if subclass.__module__.startswith(search_package):
             yield subclass
