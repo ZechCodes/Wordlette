48eb86e95857431645fa29eec0030a8cf659d64f
Zech Zimmerman <hi@zech.codes>
2022-09-15 17:18:47 -0400

    Add a yaml config file type loader

---
+++ wordlette/config/yaml_loader.py
@@ -0,0 +1,32 @@
+from functools import cache
+from pathlib import Path
+from typing import Any
+
+from .loaders import FileTypeLoader
+
+try:
+    import yaml
+except ImportError:
+    yaml = False
+
+
+class YamlFileLoader(FileTypeLoader):
+    def __init__(self, config_path: Path, file_name: str):
+        self._config_path = config_path
+        self._file_name = file_name
+
+    @property
+    @cache
+    def file_path(self) -> Path:
+        path = self._config_path / f"{self._file_name}.yaml"
+        if path.exists():
+            return path
+
+        return self._config_path / f"{self._file_name}.yml"
+
+    def exists(self) -> bool:
+        return yaml and self.file_path.exists()
+
+    def load(self) -> dict[str, Any]:
+        with self.file_path.open() as file:
+            return yaml.safe_load(file)
