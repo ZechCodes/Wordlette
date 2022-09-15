44d1b92665306e733dce1d9bab62ceb98f205fa5
Zech Zimmerman <hi@zech.codes>
2022-09-15 17:18:22 -0400

    Add a json config file type loader

---
+++ wordlette/config/json_loader.py
@@ -0,0 +1,21 @@
+import json
+from pathlib import Path
+from typing import Any
+
+from .loaders import FileTypeLoader
+
+
+class JsonFileLoader(FileTypeLoader):
+    def __init__(self, config_path: Path, file_name: str):
+        self._file_path = config_path / f"{file_name}.json"
+
+    @property
+    def file_path(self) -> Path:
+        return self._file_path
+
+    def exists(self) -> bool:
+        return self.file_path.exists()
+
+    def load(self) -> dict[str, Any]:
+        with self.file_path.open() as file:
+            return json.load(file)
