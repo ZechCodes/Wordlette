9b2e0ae58de187864832a6b077e920d2c8bc6117
Zech Zimmerman <hi@zech.codes>
2022-09-17 14:10:55 -0400

    Add more context to error messages when in dev mode

--- wordlette/app/app.py
+++ wordlette/app/app.py
@@ -1,3 +1,5 @@
+from html import escape
+
 import logging
 import uvicorn
 from bevy import Context
@@ -6,7 +8,7 @@ from pathlib import Path
 from starlette.applications import Starlette
 from starlette.responses import HTMLResponse
 from starlette.types import Receive, Scope, Send
-from typing import Callable, Iterator, Type, TypeAlias
+from typing import Any, Callable, Iterator, Type, TypeAlias
 
 from wordlette.app.states import BaseAppState, Starting
 from wordlette.config.provider import ConfigProvider
@@ -15,6 +17,7 @@ from wordlette.extensions.auto_loader import ExtensionInfo
 from wordlette.extensions.extensions import AppExtension, Extension
 from wordlette.extensions.plugins import Plugin
 from wordlette.policies.provider import PolicyProvider
+from wordlette.settings import Settings
 from wordlette.state_machine import StateMachine
 from wordlette.state_machine.machine import StateChangeEvent
 from wordlette.templates import TemplateEngine
@@ -28,26 +31,49 @@ logger = logging.getLogger("wordlette")
 
 
 class ResponseContext:
-    def __init__(self, scope: Scope, receive: Receive, send: Send):
+    def __init__(self, wordlette: BaseApp, scope: Scope, receive: Receive, send: Send):
         self.scope = scope
         self.receive = receive
         self.send = send
+        self.wordlette = wordlette
         self.app = None
 
     async def __aenter__(self):
         return self
 
+    def _build_response_error_message(self, content: str, *debug: Any):
+        message = f"<p>{content}</p>"
+        if self.wordlette.bevy.get(Settings).get("dev", False):
+            state = self.wordlette.state_machine.state
+            message += (
+                f"<h3>Current Context</h4>"
+                f"<pre>"
+                f"{escape(f'<App:{self.app!r}>')}\n"
+                f"{escape(repr(state))}\n"
+                + (
+                    "\n".join(
+                        escape(f"<{type(item).__name__}:{item}>") for item in debug
+                    )
+                )
+                + "</pre>"
+            )
+
+        return message
+
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         if exc_type:
+            message = self._build_response_error_message(exc_val.args[0], exc_val)
             self.app = app = HTMLResponse(
-                f"<h1>Wordlette Enountered An Error</h1><p>{exc_val.args[0]}</p>",
+                f"<h1>Wordlette Enountered An Error</h1>{message}",
                 status_code=500,
             )
 
         elif not self.app:
+            message = self._build_response_error_message(
+                "Wordlette could not find an application to build a response with."
+            )
             self.app = self.app = app = HTMLResponse(
-                f"<h1>Wordlette Enountered A Problem</h1><p>Wordlette could not find an application to build a "
-                f"response with.</p>",
+                f"<h1>Wordlette Enountered A Problem</h1>{message}",
                 status_code=500,
             )
 
@@ -110,7 +136,7 @@ class App(BaseApp):
         logger.info(f"Entered {event.new_state} from {event.old_state}")
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send):
-        async with ResponseContext(scope, receive, send) as response:
+        async with ResponseContext(self, scope, receive, send) as response:
             if not self.state_machine.started:
                 await self._start()
 
