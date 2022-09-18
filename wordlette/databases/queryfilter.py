0de522ca7fe2962963078aa32d41242a2e75ed8a
Zech Zimmerman <hi@zech.codes>
2022-09-18 16:52:18 -0400

    Implement the query and query result abc's

---
+++ wordlette/databases/queryfilter.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Any, Generic, Type, TypeVar
+
+from .filters import QueryFilter
+from .models import Model
+
+TModel = TypeVar("TModel", bound=Model)
+
+
+class Query(ABC, Generic[TModel]):
+    def __init__(self, model: Type[TModel], query_filter: QueryFilter | None = None):
+        self.model = model
+        self.filters = QueryFilter() if query_filter is None else query_filter
+
+    @abstractmethod
+    async def delete(self):
+        ...
+
+    @abstractmethod
+    async def fetch_all(self) -> QueryResult[TModel]:
+        ...
+
+    @abstractmethod
+    async def fetch_one(self) -> TModel:
+        ...
+
+    @abstractmethod
+    async def update(self, **changes: Any):
+        ...
+
+    def find(self, query_filter: QueryFilter) -> Query:
+        self.filters &= query_filter
+        return self
+
+
+class QueryResult(ABC, Generic[TModel]):
+    @abstractmethod
+    async def __aiter__(self):
+        ...
+
+    async def flatten(self) -> list[TModel]:
+        return [model async for model in self]
