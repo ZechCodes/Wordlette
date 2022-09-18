06a36da7b351a43a925a55e957a5ba7c3a8e7db8
Zech Zimmerman <hi@zech.codes>
2022-09-18 17:15:49 -0400

    Add query sorting

--- wordlette/databases/query.py
+++ wordlette/databases/query.py
@@ -4,15 +4,17 @@ from abc import ABC, abstractmethod
 from typing import Any, Generic, Type, TypeVar
 
 from .filters import QueryFilter
-from .models import Model
+from .models import Model, Field
 
 TModel = TypeVar("TModel", bound=Model)
+TField = TypeVar("TField", bound=Field)
 
 
 class Query(ABC, Generic[TModel]):
     def __init__(self, model: Type[TModel], query_filter: QueryFilter | None = None):
         self.model = model
         self.filter = QueryFilter() if query_filter is None else query_filter
+        self.sort_by: list[TField] = []
 
     @abstractmethod
     async def delete(self):
@@ -32,6 +34,10 @@ class Query(ABC, Generic[TModel]):
 
     def find(self, query_filter: QueryFilter) -> Query:
         self.filter &= query_filter
+        return self
+
+    def sort(self, *sorts: TField) -> Query:
+        self.sort_by.extend(sorts)
         return self
 
 
