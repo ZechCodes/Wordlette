09c2213bf47ede77c9131882c338b0c0d0e0865f
Zech Zimmerman <hi@zech.codes>
2022-09-22 11:40:43 -0400

    Use the new logging interface in the sqlalchemy driver

--- wordlette/ext/db/sqlalchemy.py
+++ wordlette/ext/db/sqlalchemy.py
@@ -1,5 +1,7 @@
 from typing import Type
 
+from bevy import bevy_method, Inject
+from wordlette import Logging
 from wordlette.databases import Database, Model
 from wordlette.databases.databases import TModel
 from wordlette.databases.filters import QueryFilter
@@ -10,8 +12,9 @@ class SQLAlchemyDatabaseDriver(Database):
     async def add(self, model: TModel):
         pass
 
-    async def connect(self):
-        print("Oh hi")
+    @bevy_method
+    async def connect(self, log: Logging = Inject):
+        log.info("Connecting to database using SQLAlchemy")
 
     async def get(self, model: Model):
         ...
