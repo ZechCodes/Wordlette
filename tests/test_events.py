e3d8d81f6be973a63ab48533ee32a7093b8e5870
Zech Zimmerman <hi@zech.codes>
2022-09-22 17:39:32 -0400

    Add tests for the new label system

--- tests/test_events.py
+++ tests/test_events.py
@@ -2,6 +2,7 @@ import pytest
 from bevy import Context
 
 from wordlette.events import Eventable
+import asyncio
 
 
 @pytest.mark.asyncio
@@ -12,7 +13,7 @@ async def test_event_dispatch():
     class TestListener(Eventable):
         ran = False
 
-        @Test.on("test-event")
+        @Test.on(name="test-event")
         async def test_event_handler(self):
             self.ran = True
 
@@ -20,5 +21,31 @@ async def test_event_dispatch():
     dispatcher = context.create(Test, cache=True)
     listener = context.create(TestListener)
 
-    await dispatcher.dispatch("test-event")
+    await dispatcher.dispatch(name="test-event", details="foo")
     assert listener.ran
+
+
+@pytest.mark.asyncio
+async def test_concurrent_event_dispatch():
+    class Test(Eventable):
+        ...
+
+    class TestListener(Eventable):
+        ran = []
+
+        @Test.on(name="test-event")
+        async def test_event_handler_a(self):
+            await asyncio.sleep(0.002)
+            self.ran.append("a")
+
+        @Test.on(name="test-event")
+        async def test_event_handler_b(self):
+            await asyncio.sleep(0.001)
+            self.ran.append("b")
+
+    context = Context.factory()
+    dispatcher = context.create(Test, cache=True)
+    listener = context.create(TestListener)
+
+    await dispatcher.dispatch(name="test-event", details="foo")
+    assert listener.ran == ["b", "a"]
