662b23b9c26ab1e4f70792d85c970e4d6c50c44a
ZechCodes <hi@zech.codes>
2023-07-07 17:44:24 -0400

    Make bootstrap state transition immediately

--- wordlette/cms/states/bootstrap.py
+++ wordlette/cms/states/bootstrap.py
@@ -6,6 +6,7 @@ from wordlette.states import State
 class BootstrapState(State):
     async def enter_state(self):
         self.value = self._bootstrap_error_app
+        return True
 
     async def _bootstrap_error_app(self, scope, receive, send):
         response = PlainTextResponse("The Wordlette CMS is starting.", status_code=200)
