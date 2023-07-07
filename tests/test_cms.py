7b97a045e6d669001ed80f5e06309b168220b226
ZechCodes <hi@zech.codes>
2023-07-07 17:43:56 -0400

    Update boostrap test for immediate transitions

--- tests/test_cms.py
+++ tests/test_cms.py
@@ -2,6 +2,7 @@ import pytest
 from starlette.testclient import TestClient
 
 from wordlette.cms import CMSApp
+from wordlette.cms.states import BootstrapState
 
 
 @pytest.fixture
@@ -9,6 +10,11 @@ def client():
     return TestClient(CMSApp())
 
 
-def test_cms_bootstrap_app(client):
+@pytest.mark.asyncio
+async def test_cms_bootstrap_app():
+    state = BootstrapState()
+    await state.enter_state()
+
+    client = TestClient(state.value)
     assert client.get("/").status_code == 200
     assert client.get("/testing").status_code == 200
