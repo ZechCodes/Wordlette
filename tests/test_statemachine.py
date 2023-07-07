7f1b8cfcfa1676333f3e97969590cd4130474138
ZechCodes <hi@zech.codes>
2023-07-07 15:42:35 -0400

    Add tests for a statemachine

---
+++ tests/test_statemachine.py
@@ -0,0 +1,48 @@
+from typing import Type
+
+import pytest
+
+from wordlette.options import Option
+from wordlette.states import State
+
+
+@pytest.mark.asyncio
+async def test_statemachine():
+    entered_a = False
+    entered_b = False
+
+    class StateA(State):
+        async def enter_state(self):
+            nonlocal entered_a
+            entered_a = True
+
+        async def next_state(self) -> "Option[Type[StateB]]":
+            return Option.Value(StateB)
+
+    class StateB(State):
+        async def enter_state(self):
+            nonlocal entered_b
+            entered_b = True
+
+    machine = await StateA.start()
+    await machine.next()
+    assert entered_a
+    assert entered_b
+
+
+@pytest.mark.asyncio
+async def test_state_value():
+    class StateA(State[int]):
+        async def enter_state(self):
+            self.value = 1
+
+        async def next_state(self) -> "Option[Type[StateB]]":
+            return Option.Value(StateB)
+
+    class StateB(State[int]):
+        async def enter_state(self):
+            self.value = 2
+
+    machine = await StateA.start()
+    await machine.next()
+    assert machine.value == 2
