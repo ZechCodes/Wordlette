93721cdaf78fffcca82065ba162de34c188e75fe
ZechCodes <hi@zech.codes>
2023-07-10 19:23:00 -0400

    Add basic logging to states

--- wordlette/state_machines/states.py
+++ wordlette/state_machines/states.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
+import logging
 from typing import Generic, Type, TypeVar, ParamSpec, Callable, Awaitable
 
 from wordlette.options import Option
 from wordlette.state_machines.predicates import always
+
+logger = logging.getLogger("StateMachine")
 
 _T = TypeVar("_T")
 _P = ParamSpec("_P")
@@ -12,7 +15,17 @@ class RequestCycle:
     pass
 
 
-class State(Generic[T], ABC):
+class StateABCMeta(ABCMeta):
+    def __repr__(cls):
+        transitions = (
+            ", ".join(sorted(repr(transition) for transition in cls.transitions))
+            if cls.transitions
+            else ""
+        )
+        return f"<Class:State:{cls.__name__} [{transitions}]>"
+
+
+class State(Generic[_T], metaclass=StateABCMeta):
     transitions = {}
 
     def __init_subclass__(cls, **kwargs):
@@ -31,6 +44,7 @@ class State(Generic[T], ABC):
 
     async def get_next_state(self) -> "Option[Type[State[_T]]]":
         for state, predicate in self.transitions.items():
+            logger.debug(f"Checking {state} with {predicate}")
             if await predicate():
                 return Option.Value(state)
 
@@ -42,6 +56,7 @@ class State(Generic[T], ABC):
     ):
         state_cls = cls
         if not cls.transitions:
+            logger.debug(f"CREATING TRANSITION DICT FOR {cls} {id(cls)}")
             state_cls = type(cls.__name__, (cls,), {})
 
         state_cls.transitions[state] = when
