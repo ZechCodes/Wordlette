b213054ba7ab15266e3255c81c9a17523b2045b9
ZechCodes <hi@zech.codes>
2023-07-12 14:44:21 -0400

    Cycle the creating config state if there’s a config

--- wordlette/app/states/creating_config.py
+++ wordlette/app/states/creating_config.py
@@ -8,6 +8,9 @@ from wordlette.state_machines import State
 class CreatingConfig(State):
     @inject
     async def enter_state(self, app: WordletteApp = dependency()):
+        if await self.has_config():
+            return self.cycle()
+
         app.set_router(PlainTextResponse("Creating Config File", status_code=200))
 
     @staticmethod
