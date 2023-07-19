8aef2697519475cf0baea009004ff513006f9d6a
ZechCodes <hi@zech.codes>
2023-07-19 17:38:22 -0400

    Add a config manager to the application launch

--- wordlette/app/__init__.py
+++ wordlette/app/__init__.py
@@ -1,4 +1,5 @@
 from wordlette.app.app import WordletteApp
+from wordlette.configs.managers import ConfigManager
 
 
 def app(*args, **kwargs) -> WordletteApp:
@@ -23,4 +24,19 @@ def app(*args, **kwargs) -> WordletteApp:
             CreatingConfig.goes_to(ConnectingDB),
             ConnectingDB.goes_to(ServingApp),
         )
+        ),
+        config_manager=_create_config_manager(),
     )
+
+
+def _create_config_manager():
+    from wordlette.configs.json_handlers import JsonHandler
+    from wordlette.configs.toml_handlers import TomlHandler
+    from wordlette.configs.yaml_handlers import YamlHandler
+
+    manager = ConfigManager([JsonHandler])
+    for handler in [TomlHandler, YamlHandler]:
+        if handler.supported():
+            manager.add_handler(handler)
+
+    return manager
