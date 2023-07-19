7a516ad90d4a33ba3cc58dd231329e37f50b01e9
ZechCodes <hi@zech.codes>
2023-07-19 13:49:14 -0400

    Add extension type and helper functions

---
+++ wordlette/extensions.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from importlib import import_module
+from pathlib import Path
+
+
+@dataclass
+class Extension:
+    name: str
+    import_path: str
+
+    def load_extension(self):
+        return import_module(self.import_path)
+
+
+def scan_directory_for_public_modules(directory: Path) -> set[Path]:
+    return {
+        path
+        for path in directory.iterdir()
+        if (
+            not path.name.startswith("_")
+            and (path.suffix in {".py", ".pyc"} or path.is_dir())
+        )
+    }
+
+
+def get_extensions_in_directory(directory: Path) -> list[Extension]:
+    return [
+        Extension(path.stem, f"{path.parent.name}.{path.stem}")
+        for path in scan_directory_for_public_modules(directory)
+    ]
