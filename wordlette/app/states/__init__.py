334e697f2f955348cf119aefc8d0c79650026b4c
ZechCodes <hi@zech.codes>
2023-07-19 17:37:49 -0400

    Add loading config state to package imports

--- wordlette/app/states/__init__.py
+++ wordlette/app/states/__init__.py
@@ -2,4 +2,5 @@ from wordlette.app.states.bootstrapping_app import BootstrappingApp
 from wordlette.app.states.connecting_db import ConnectingDB
 from wordlette.app.states.creating_config import CreatingConfig
 from wordlette.app.states.load_core_extensions import LoadCoreExtensions
+from wordlette.app.states.loading_config import LoadingConfig
 from wordlette.app.states.serving_app import ServingApp
