2572daab686e4b2cea60bc198ebb087534e126f1
ZechCodes <hi@zech.codes>
2023-07-28 10:37:07 -0400

    Route manager now acts like an actual route manager

--- wordlette/middlewares/state_router.py
+++ wordlette/middlewares/state_router.py
@@ -1,25 +1,64 @@
+from typing import Sequence, Protocol, runtime_checkable, Type
+
 from bevy import get_repository
-from starlette.responses import PlainTextResponse
-from starlette.types import ASGIApp, Scope, Send, Receive
+from starlette.routing import Router
+from starlette.types import Scope, Send, Receive
 
 from wordlette.app.app import StartupEvent
 from wordlette.events import Observer
 from wordlette.middlewares import Middleware
+from wordlette.routes import Route
 from wordlette.state_machines import StateMachine
+
+
+@runtime_checkable
+class RouterProtocol(Protocol):
+    async def __call__(self, scope: Scope, receive: Receive, send: Send):
+        ...
+
+    def add_route(
+        self,
+        path: str,
+        route: Route,
+        methods: Sequence[str],
+        name: str,
+        *args,
+        **kwargs,
+    ):
+        ...
 
 
 class RouteManager:
     def __init__(self):
         super().__init__()
-        self._router = PlainTextResponse("No router is configured", status_code=500)
+        self._router: RouterProtocol | None = None
 
     @property
-    def router(self) -> ASGIApp:
+    def router(self) -> RouterProtocol:
+        if not self._router:
+            self.create_router()
+
         return self._router
 
     @router.setter
     def router(self, router):
+        if not isinstance(router, RouterProtocol):
+            raise TypeError(f"router must adhere to the {RouterProtocol.__name__}")
+
         self._router = router
+
+    def add_route(self, route: Type[Route]):
+        self.router.add_route(
+            route.path,
+            route(),
+            route.methods,
+            route.name,
+        )
+
+    def create_router(self, *routes: Type[Route]):
+        self.router = Router()
+        for route in routes:
+            self.add_route(route)
 
 
 class StateRouterMiddleware(Middleware, Observer):
