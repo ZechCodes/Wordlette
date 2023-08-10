0205f460893c900ef39263de04569cac5936575e
ZechCodes <hi@zech.codes>
2023-08-10 17:58:47 -0400

    Create a simple type for composing an event dispatch

---
+++ wordlette/events/dispatchable.py
@@ -0,0 +1,10 @@
+from wordlette.events.dispatch import EventDispatch
+
+
+class Dispatchable:
+    __event_dispatch__: EventDispatch
+
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls, *args, **kwargs)
+        instance.__event_dispatch__ = EventDispatch()
+        return instance
