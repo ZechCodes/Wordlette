e972d9f192b05cca7d0dea2946bc811569629ad0
ZechCodes <hi@zech.codes>
2023-08-11 15:07:57 -0400

    Make it easier to import event dispatch and event

--- wordlette/events/__init__.py
+++ wordlette/events/__init__.py
@@ -1,2 +1,4 @@
+from wordlette.events.dispatch import EventDispatch
+from wordlette.events.events import Event
 from wordlette.events.observable import Observable
 from wordlette.events.observer import Observer
