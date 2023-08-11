d82cea3df0494e03fb425b890d8bf0f5a13ffae0
ZechCodes <hi@zech.codes>
2023-08-11 19:15:23 -0400

    Make the starting event observable

--- wordlette/core/states/starting.py
+++ wordlette/core/states/starting.py
@@ -1,12 +1,12 @@
-from bevy import inject, dependency
+from bevy import inject
 
 from wordlette.core.events import StartupEvent
-from wordlette.events import EventDispatch
+from wordlette.events import Observable
 from wordlette.state_machines import State
 
 
-class Starting(State):
+class Starting(State, Observable):
     @inject
-    async def enter_state(self, events: EventDispatch = dependency()):
-        await events.emit(StartupEvent())
+    async def enter_state(self):
+        await self.emit(StartupEvent())
         return self.cycle()
