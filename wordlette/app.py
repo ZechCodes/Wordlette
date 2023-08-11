df7dcf5a7d6be164310bceb8bef4797ca3d7ab08
ZechCodes <hi@zech.codes>
2023-08-11 15:38:59 -0400

    Move the app object to the app module

---
+++ wordlette/app.py
@@ -0,0 +1,12 @@
+from wordlette.core import WordletteApp
+from wordlette.core.states import SettingUp, Starting, Serving
+from wordlette.middlewares.router_middleware import RouterMiddleware
+from wordlette.state_machines import StateMachine
+
+app = WordletteApp(
+    middleware=[RouterMiddleware],
+    state_machine=StateMachine(
+        Starting.goes_to(SettingUp, when=SettingUp.needs_setup),
+        Starting.goes_to(Serving),
+    ),
+)
--- wordlette/core/__init__.py
+++ wordlette/core/__init__.py
@@ -1,19 +1,5 @@
-from wordlette.app.app import WordletteApp
 from wordlette.configs.managers import ConfigManager
-from wordlette.middlewares.router_middleware import RouterMiddleware
-
-
-def app(*args, **kwargs) -> WordletteApp:
-    from wordlette.state_machines import StateMachine
-    from wordlette.app.states import Starting, SettingUp, Serving
-
-    return WordletteApp(
-        middleware=[RouterMiddleware],
-        state_machine=StateMachine(
-            Starting.goes_to(SettingUp, when=SettingUp.needs_setup),
-            Starting.goes_to(Serving),
-        ),
-    )
+from wordlette.core.app import WordletteApp
 
 
 def _create_config_manager():
