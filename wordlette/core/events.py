3127ca1bc955717229fb3665fc17443c5589e8e2
ZechCodes <hi@zech.codes>
2023-08-11 19:15:33 -0400

    Give events custom names

--- wordlette/core/events.py
+++ wordlette/core/events.py
@@ -6,14 +6,18 @@ from wordlette.events import Event
 
 
 class StartupEvent(Event):
-    ...
+    __event_name__ = "Startup"
 
 
 @dataclass
 class LifespanStartupEvent(Event):
+    __event_name__ = "Lifespan.Startup"
+
     scope: Scope
 
 
 @dataclass
 class LifespanShutdownEvent(Event):
+    __event_name__ = "Lifespan.Shutdown"
+
     scope: Scope
