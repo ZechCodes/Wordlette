bfbab78c821a3507a0fb15ec693787eddf1e79c1
ZechCodes <hi@zech.codes>
2023-08-11 17:00:05 -0400

    Emit a startup event

--- wordlette/core/states/starting.py
+++ wordlette/core/states/starting.py
@@ -1,6 +1,12 @@
+from bevy import inject, dependency
+
+from wordlette.core.events import StartupEvent
+from wordlette.events import EventDispatch
 from wordlette.state_machines import State
 
 
 class Starting(State):
-    async def enter_state(self):
+    @inject
+    async def enter_state(self, events: EventDispatch = dependency()):
+        await events.emit(StartupEvent())
         return self.cycle()
