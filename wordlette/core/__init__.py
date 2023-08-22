9052979348499132e6fd22442d9ecf881edf9d42
ZechCodes <hi@zech.codes>
2023-08-22 10:14:54 -0400

    Remove unnecessary route sub class from core

--- wordlette/core/__init__.py
+++ wordlette/core/__init__.py
@@ -1,3 +1,2 @@
 from wordlette.core.app import WordletteApp
-from wordlette.core.routes import Route
 from wordlette.core.setup_routes import SetupRoute
--- wordlette/core/routes.py
+++
@@ -1,9 +0,0 @@
-from wordlette.routes import Route as _Route
-
-
-class Route(_Route):
-    """All subclasses of this abstract route type are automatically mounted by the Serving state."""
-
-    class __metadata__:
-        abstract = True
-        registry = set()
