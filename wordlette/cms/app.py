d108c109ac1e4760363a14105046ef7ad11572ee
ZechCodes <hi@zech.codes>
2023-08-22 21:00:02 -0400

    Implement the CMS app object

---
+++ wordlette/cms/app.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from typing import Annotated
+
+from bevy import get_repository
+
+from wordlette.cms.states.setup import Setup
+from wordlette.core import WordletteApp
+from wordlette.middlewares.router_middleware import RouterMiddleware
+from wordlette.state_machines import StateMachine
+
+
+def _setup_repository():
+    repository = get_repository()
+    repository.set(
+        Annotated[Path, "package-resources"], Path(__file__).parent / "resources"
+    )
+
+
+_setup_repository()
+app = WordletteApp(
+    middleware=[RouterMiddleware],
+    state_machine=StateMachine(Setup),
+)
