77c2a96b1bd647dc88179d3e3945e9382db501d5
ZechCodes <hi@zech.codes>
2023-08-27 20:35:10 -0400

    Add a CLI debug flag

--- wordlette/cms/cli.py
+++ wordlette/cms/cli.py
@@ -1,16 +1,16 @@
 def run():
     import sys
 
-    match sys.argv:
-        case [_, "serve"]:
-            _start_server()
+    match (*map(str.casefold, sys.argv),):
+        case (_, "serve", *args):
+            _start_server(debug="--debug" in args or "-d" in args)
 
         case _:
             raise RuntimeError("Invalid command")
 
 
-def _start_server():
-    from wordlette.cms.app import app
+def _start_server(**settings):
+    from wordlette.cms.app_bootstrap import create_app
     import uvicorn
 
-    uvicorn.run(app, port=8000, log_level="info")
+    uvicorn.run(create_app(**settings), port=8000, log_level="info")
