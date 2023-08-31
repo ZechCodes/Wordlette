e37ba320d857ebc3fe42bff424a7f931b7505517
ZechCodes <hi@zech.codes>
2023-08-31 16:16:29 -0400

    Implement the config creation page

--- wordlette/cms/states/setup.py
+++ wordlette/cms/states/setup.py
@@ -5,7 +5,7 @@ from wordlette.middlewares.router_middleware import RouteManager
 from wordlette.requests import Request
 from wordlette.routes import Route
 from wordlette.state_machines import State
-from wordlette.utils.dependency_injection import inject, AutoInject
+from wordlette.utils.dependency_injection import inject, AutoInject, inject_dependencies
 
 
 class _SetupRoute(Route):
@@ -22,31 +22,50 @@ class Index(_SetupRoute):
             "index.html",
             title="Wordlette",
             subtitle="Setting Up Your Site",
-            next_page_url=self._get_next_page(),
+            next_page_url=_get_next_page(),
         )
-
-    def _get_next_page(
-        self,
-        config: ConfigManager @ inject,
-        router: RouteManager @ inject,
-        settings_filename: str @ AppSetting("settings-filename"),
-        working_directory: str @ AppSetting("working-directory"),
-    ):
-        settings_path = config.find_config_file(settings_filename, working_directory)
-        if settings_path is None:
-            return router.url_for(CreateSettingsFile)
-
-        return router.url_for(SetupComplete)
 
 
 class CreateSettingsFile(_SetupRoute):
     path = "/create-settings-file"
 
     async def get_setup_page(self, _: Request.Get):
+        subtitle = "Create Your Settings File"
         return Template(
             "create-settings-file.html",
-            title="Wordlette",
-            subtitle="Create Settings File",
+            title=f"Wordlette: {subtitle}",
+            heading="Setup",
+            subtitle=subtitle,
+        )
+
+    async def create_settings_file(
+        self,
+        request: Request.Post,
+        config: ConfigManager @ inject,
+        settings_filename: str @ AppSetting("settings-filename"),
+        working_directory: str @ AppSetting("working-directory"),
+    ):
+        settings = await request.form()
+        path = config.write_config_file(
+            settings_filename,
+            working_directory,
+            {
+                "site": {
+                    "domain": settings["domain-name"],
+                    "https": settings["uses-https"].casefold() in {"on"},
+                    "name": settings["site-name"],
+                }
+            },
+        )
+        subtitle = "Your Settings File Has Been Created"
+        return Template(
+            "create-settings-file.html",
+            title="Wordlette: {subtitle}",
+            heading="Setup",
+            subtitle=subtitle,
+            created_settings_file=True,
+            file_path=path,
+            next_page_url=_get_next_page(),
         )
 
 
@@ -69,3 +88,17 @@ class Setup(State, AutoInject):
     ):
         theme_manager.set_theme(theme_manager.wordlette_res / "themes" / "setup")
         _SetupRoute.register_routes(route_manager.router)
+
+
+@inject_dependencies
+def _get_next_page(
+    config: ConfigManager @ inject = None,
+    router: RouteManager @ inject = None,
+    settings_filename: str @ AppSetting("settings-filename") = None,
+    working_directory: str @ AppSetting("working-directory") = None,
+):
+    settings_path = config.find_config_file(settings_filename, working_directory)
+    if settings_path is None:
+        return router.url_for(CreateSettingsFile)
+
+    return router.url_for(SetupComplete)
