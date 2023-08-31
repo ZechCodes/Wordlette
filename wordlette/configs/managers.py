f4a6a27df0ac2351231bbec31136b6b2c4474f2a
ZechCodes <hi@zech.codes>
2023-08-31 16:15:52 -0400

    More intelligently find a config handler for writing

--- wordlette/configs/managers.py
+++ wordlette/configs/managers.py
@@ -1,7 +1,8 @@
 import logging
 from pathlib import Path
-from typing import Type, Sequence, TypeVar, Any, Callable
+from typing import Type, TypeVar, Any, Callable, Iterable, BinaryIO
 
+from wordlette.configs.exceptions import ConfigCannotCreateFiles
 from wordlette.configs.handlers import ConfigHandler
 from wordlette.core.exceptions import ConfigFileNotFound
 
@@ -11,15 +12,16 @@ T = TypeVar("T")
 
 
 class ConfigManager:
-    def __init__(self, handlers: Sequence[Type[ConfigHandler]] = ()):
-        self._handlers: dict[str, ConfigHandler] = {}
+    def __init__(self, handlers: Iterable[Type[ConfigHandler]] = ()):
+        self._handler_extensions: dict[str, ConfigHandler] = {}
+        self._handlers: list[ConfigHandler] = []
         self._config: dict[str, Any] = {}
 
         self._register_handlers(handlers)
 
     @property
     def valid_extensions(self) -> set[str]:
-        return set(self._handlers.keys())
+        return set(self._handler_extensions.keys())
 
     def get(
         self, key: str = "", constructor: Callable[[dict[str, Any]], T] | None = None
@@ -31,7 +33,7 @@ class ConfigManager:
         return value
 
     def find_config_file(self, name: str, directory: Path) -> Path | None:
-        for extension in self._handlers:
+        for extension in self._handler_extensions:
             path = directory / f"{name}.{extension}"
             if path.exists():
                 return path
@@ -43,32 +45,60 @@ class ConfigManager:
                 f"Could not find any config files that matched the naming scheme '{name}.*' in '{directory}'."
             )
 
-        handler = self._handlers[path.suffix[1:].casefold()]
+        handler = self._handler_extensions[path.suffix[1:].casefold()]
         with path.open("rb") as open_file:
             self._config = handler.load(open_file)
 
         logger.debug(f"Loaded config file '{path}'.")
 
-    def write_config_file(self, name: str, directory: Path, data: T):
+    def write_config_file(self, name: str, directory: Path, data: T) -> Path:
         if path := self.find_config_file(name, directory):
-            handler = self._handlers[path.suffix[1:].casefold()]
+            logger.debug(f"Found existing config file {str(path)!r}.")
+            with path.open("wb") as open_file:
+                self._write_file_with_handler(
+                    data,
+                    open_file,
+                    self._handler_extensions[path.suffix[1:].casefold()],
+                )
+
         else:
-            extension = list(self._handlers)[0]
-            handler = self._handlers[extension]
-            path = directory / f"{name}.{extension}"
+            path = directory / f"{name}.tmp"
+            with path.open("wb") as open_file:
+                for handler in self._handlers:
+                    logger.debug(
+                        f"Attempting to create config using {type(handler).__name__}."
+                    )
+                    try:
+                        self._write_file_with_handler(data, open_file, handler)
+                    except ConfigCannotCreateFiles:
+                        open_file.truncate()
+                        continue
+                    else:
+                        extension = min(handler.extensions)
+                        new_name = f"{name}.{extension}"
+                        path.rename(new_name)
+                        path = directory / new_name
+                        break
+
+        logger.debug(f"Wrote to config file {str(path)!r}.")
 
         self._config = data
-        with path.open("wb") as open_file:
-            handler.write(data, open_file)
+        return path
 
-    def add_handler(self, handler: Type[ConfigHandler]):
+    def _write_file_with_handler(
+        self, data: dict[str, Any], file: BinaryIO, handler: ConfigHandler
+    ):
+        handler.write(data, file)
+
+    def add_handler(self, handler_type: Type[ConfigHandler]):
+        self._handlers.append(handler := handler_type())
         for extension in handler.extensions:
-            self._handlers[extension.casefold()] = handler()
+            self._handler_extensions[extension.casefold()] = handler
 
     def remove_handler(self, handler: Type[ConfigHandler]):
         for extension in handler.extensions:
-            del self._handlers[extension.casefold()]
+            del self._handler_extensions[extension.casefold()]
 
-    def _register_handlers(self, handlers: Sequence[Type[ConfigHandler]]):
+    def _register_handlers(self, handlers: Iterable[Type[ConfigHandler]]):
         for handler in handlers:
             self.add_handler(handler)
