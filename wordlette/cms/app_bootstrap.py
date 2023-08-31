e92a354459ef904f659c947b62bd0eee7f261a29
ZechCodes <hi@zech.codes>
2023-08-31 16:13:09 -0400

    Bootstrap config handlers

--- wordlette/cms/app_bootstrap.py
+++ wordlette/cms/app_bootstrap.py
@@ -5,9 +5,24 @@ from bevy import get_repository
 
 from wordlette.cms.extensions.error_pages import ErrorPages
 from wordlette.cms.states.setup import Setup
+from wordlette.configs.managers import ConfigManager
 from wordlette.core import WordletteApp
 from wordlette.middlewares.router_middleware import RouterMiddleware
 from wordlette.state_machines import StateMachine
+
+
+def _get_config_handlers():
+    from wordlette.configs import JsonHandler, TomlHandler, YamlHandler
+
+    yield from (
+        handler
+        for handler in (
+            YamlHandler,
+            JsonHandler,
+            TomlHandler,
+        )
+        if not hasattr(handler, "supported") or handler.supported()
+    )
 
 
 def _setup_repository():
@@ -15,6 +30,8 @@ def _setup_repository():
     repository.set(
         Annotated[Path, "package-resources"], Path(__file__).parent / "resources"
     )
+
+    repository.set(ConfigManager, ConfigManager(_get_config_handlers()))
 
 
 def create_app(**settings):
