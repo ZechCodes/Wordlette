9b418893e1e24acc7fc4a5e41e4f0dff6bf0b707
ZechCodes <hi@zech.codes>
2023-09-15 17:10:15 -0400

    Make the validation exception more advanced

--- wordlette/forms/exceptions.py
+++ wordlette/forms/exceptions.py
@@ -1,11 +1,20 @@
+import wordlette.forms
 from wordlette.base_exceptions import BaseWordletteException
 
 
 class FormValidationError(BaseWordletteException):
     """Raised when a form fails to validate."""
 
-    name = "Form Validation Error(s)"
-
-    def __init__(self, message: str, errors: dict[str, Exception]):
+    def __init__(
+        self, message: str, errors: dict[str, Exception], form: "wordlette.forms.Form"
+    ):
         super().__init__(message)
         self.errors = errors
+        self.form = form
+
+        for field_name, error in errors.items():
+            self.add_note(f"{field_name} - {error}")
+
+    @property
+    def name(self):
+        return f"Form Validation Error{'s' if len(self.errors) > 1 else ''}"
