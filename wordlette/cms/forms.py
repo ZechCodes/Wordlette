bfdc0aff37e4838ca22cdbc2f974115f605540d8
ZechCodes <hi@zech.codes>
2023-09-18 16:02:22 -0400

    Correctly implement the form view

--- wordlette/cms/forms.py
+++ wordlette/cms/forms.py
@@ -1,11 +1,16 @@
 from bevy import dependency, inject
+from markupsafe import Markup
 
 from wordlette.cms.themes import ThemeManager
 from wordlette.forms import Form as _Form
+from wordlette.forms.views import FormView as _FormView
+from wordlette.utils.contextual_methods import contextual_method
 
 
-class Form(_Form):
-    __form_template__: str = "form.html"
+class FormView(_FormView):
+    def __init__(self, template: str, *args):
+        super().__init__(*args)
+        self.template = template
 
     @inject
     def render(
@@ -15,5 +20,30 @@ class Form(_Form):
         __tm: ThemeManager = dependency(),
         **context,
     ) -> str:
-        template = __template or self.__form_template__
-        return __tm.render_template(template, form=self, **context)
+        template = __template or self.template
+        return Markup(__tm.render_template(template, form=self, **context))
+
+
+class Form(_Form):
+    __form_template__: str = "form.html"
+    __form_view_type__ = FormView
+
+    @contextual_method
+    def view(self) -> FormView:
+        return self.__form_view_type__(
+            self.__form_template__,
+            self.__form_fields__,
+            self.buttons,
+            self.__field_values__,
+            self._validate_fields(),
+        )
+
+    @view.classmethod
+    def view(cls) -> FormView:
+        return cls.__form_view_type__(
+            cls.__form_template__,
+            cls.__form_fields__,
+            cls.buttons,
+            {},
+            {},
+        )
