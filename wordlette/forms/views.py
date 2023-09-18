3c6c2a4cc7ce42fa546e6a83cd3e655a2a96af7f
ZechCodes <hi@zech.codes>
2023-09-18 14:45:11 -0400

    Compose fields, buttons, & labels on demand

--- wordlette/forms/views.py
+++ wordlette/forms/views.py
@@ -1,6 +1,7 @@
 from typing import Iterable, Any
 
 from wordlette.forms import Field
+from wordlette.forms.elements import Element, LabelElement
 from wordlette.forms.field_types import Button, not_set
 
 
@@ -12,14 +13,47 @@ class FormView:
         values: dict[str, Any],
         errors: dict[str, Exception],
     ):
-        self.fields = fields
-        self.buttons = buttons
+        self._buttons = None
+        self._fields = None
+        self._labels = None
+
         self.errors = errors
         self.values = values
 
-    def compose(self):
-        for name, field in self.fields.items():
-            yield from field.compose(self.values.get(name, not_set))
+        self.raw_buttons = buttons
+        self.raw_fields = fields
 
-        for button in self.buttons:
-            yield from button.compose()
+    @property
+    def buttons(self) -> list[Element]:
+        if self._buttons is None:
+            self._buttons = self._compose_buttons(self.raw_buttons)
+
+        return self._buttons
+
+    @property
+    def fields(self) -> dict[str, Element]:
+        if self._fields is None:
+            self._fields = self._compose_fields(self.raw_fields, self.values)
+
+        return self._fields
+
+    @property
+    def labels(self) -> dict[str, LabelElement]:
+        if self._labels is None:
+            self._labels = self._compose_labels(self.raw_fields)
+
+        return self._labels
+
+    def _compose_buttons(self, buttons: Iterable[Button]) -> list[Element]:
+        return [button.compose() for button in buttons]
+
+    def _compose_fields(
+        self, fields: dict[str, Field], values: dict[str, Any]
+    ) -> dict[str, Element]:
+        return {
+            name: field.compose(values.get(name, not_set))
+            for name, field in fields.items()
+        }
+
+    def _compose_labels(self, fields: dict[str, Field]) -> dict[str, LabelElement]:
+        return {name: field.compose_label() for name, field in fields.items()}
