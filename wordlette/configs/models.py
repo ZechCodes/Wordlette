6a1d50258cb8d416d75015cf59f3f05b80fac3e2
ZechCodes <hi@zech.codes>
2023-09-21 17:35:21 -0400

    Load config keys from the class constructor

--- wordlette/configs/models.py
+++ wordlette/configs/models.py
@@ -6,6 +6,10 @@ from wordlette.configs.managers import ConfigManager
 class ConfigModel:
     __config_key__: str
 
+    def __init_subclass__(cls, **kwargs):
+        cls.__config_key__ = kwargs.pop("key", getattr(cls, "__config_key__", None))
+        super().__init_subclass__(**kwargs)
+
     @classmethod
     @inject
     def __bevy_constructor__(cls, config: ConfigManager = dependency()):
