72ed3731135ee21bf1a748eaf8da6a53b84a7a23
ZechCodes <hi@zech.codes>
2023-09-21 12:20:13 -0400

    Add a context manager for capturing errors

---
+++ wordlette/utils/suppress_with_capture.py
@@ -0,0 +1,15 @@
+class SuppressWithCapture:
+    def __init__(self, *exceptions):
+        self._exceptions = exceptions
+        self._captured = None
+
+    @property
+    def captured(self) -> Exception | None:
+        return self._captured
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self._captured = exc_value
+        return exc_type is not None and issubclass(exc_type, self._exceptions)
