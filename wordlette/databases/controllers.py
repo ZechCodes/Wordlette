bf3a4acf8a680f4bf1d9f0c851a8c81bae78df29
ZechCodes <hi@zech.codes>
2023-09-21 17:54:24 -0400

    Fix incorrect lookups for drivers

--- wordlette/databases/controllers.py
+++ wordlette/databases/controllers.py
@@ -1,6 +1,6 @@
 from bevy import get_repository
 
-from wordlette.databases.drivers import AbstractDatabaseDriver
+from wordlette.databases.drivers import AbstractDatabaseDriver, DatabaseDriver
 from wordlette.databases.settings_models import DatabaseSettings
 from wordlette.databases.statuses import DatabaseStatus, DatabaseSuccessStatus
 from wordlette.utils.dependency_injection import inject, AutoInject
@@ -20,14 +20,14 @@ class DatabaseController(AutoInject):
     async def connect(
         self, db_settings: DatabaseSettings @ inject = None
     ) -> DatabaseStatus:
-        driver = AbstractDatabaseDriver.__drivers__.get(db_settings.driver)
+        driver = DatabaseDriver.__drivers__.get(db_settings.driver)
         if driver is None:
             raise RuntimeError(
                 f"Could not find a driver that matches {db_settings.driver!r}"
             )
 
         self._driver = driver()
-        get_repository().set(AbstractDatabaseDriver, self._driver)
+        get_repository().set(DatabaseDriver, self._driver)
         return await self._driver.connect()
 
     async def disconnect(self) -> DatabaseStatus:
