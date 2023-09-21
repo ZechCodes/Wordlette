ecadb3756e0c753ca09a20b1d9dd105592d3f3b3
ZechCodes <hi@zech.codes>
2023-09-21 17:41:37 -0400

    Add a test for connecting to a database

---
+++ tests/test_databases.py
@@ -0,0 +1,80 @@
+from typing import Type
+
+import pytest
+from bevy import get_repository, Repository
+
+from wordlette.configs.managers import ConfigManager
+from wordlette.databases.drivers import DatabaseDriver
+from wordlette.databases.models import DatabaseModel
+from wordlette.databases.predicates import DatabasePredicate
+from wordlette.databases.statuses import DatabaseSuccessStatus, DatabaseStatus
+from wordlette.utils.at_annotateds import AtProvider
+
+
+class DummyDriver(DatabaseDriver, driver_name="dummy"):
+    def __init__(self):
+        self._connected = False
+
+    @property
+    def connected(self) -> bool:
+        return self._connected
+
+    async def add(self, *items: DatabaseModel) -> DatabaseStatus:
+        pass
+
+    async def count(self, *predicates: DatabasePredicate) -> int:
+        pass
+
+    async def filter(self, *predicates: DatabasePredicate) -> DatabaseStatus:
+        pass
+
+    async def get(self, *predicates: DatabasePredicate) -> DatabaseStatus:
+        pass
+
+    async def remove(self, *items: DatabaseModel) -> DatabaseStatus:
+        pass
+
+    async def sync_schema(self, models: set[Type[DatabaseModel]]) -> DatabaseStatus:
+        pass
+
+    async def update(self, *items: DatabaseModel) -> DatabaseStatus:
+        pass
+
+    async def connect(self) -> DatabaseSuccessStatus:
+        self._connected = True
+        return DatabaseSuccessStatus(None)
+
+    async def disconnect(self) -> DatabaseSuccessStatus:
+        self._connected = False
+        return DatabaseSuccessStatus(None)
+
+
+class DummyConfigManager(ConfigManager):
+    def __init__(self):
+        super().__init__()
+        self._config = {
+            "database": {
+                "driver": "dummy",
+            }
+        }
+
+
+@pytest.fixture(scope="function", autouse=True)
+def reset_bevy_repository():
+    repo = Repository.factory()
+    repo.add_providers(AtProvider())
+    repo.set(ConfigManager, DummyConfigManager())
+    Repository.set_repository(repo)
+
+
+@pytest.mark.asyncio
+async def test_connect():
+    from wordlette.databases.controllers import DatabaseController
+
+    controller = DatabaseController()
+    assert not controller.connected
+
+    status = await controller.connect()
+    assert status == DatabaseSuccessStatus(None)
+    assert controller.connected
+    assert isinstance(get_repository().get(DatabaseDriver), DummyDriver)
