f41ab22139d3c9bc225eefabcb445784215c691b
ZechCodes <hi@zech.codes>
2023-09-26 16:29:18 -0400

    Implement the database setup page

--- wordlette/cms/states/setup/routes/configure_database.py
+++ wordlette/cms/states/setup/routes/configure_database.py
@@ -1,38 +1,40 @@
+import logging
+from typing import cast
+
+from starlette.responses import RedirectResponse
+
 from wordlette.cms.forms import Form
 from wordlette.cms.states.setup.enums import SetupCategory, SetupStatus
 from wordlette.cms.states.setup.route_types import SetupRoute
 from wordlette.cms.themes import Template
 from wordlette.configs.managers import ConfigManager
 from wordlette.core.app import AppSetting
+from wordlette.databases.drivers import DatabaseDriver
+from wordlette.databases.settings_models import DatabaseSettings
 from wordlette.forms.exceptions import FormValidationError
-from wordlette.forms.field_types import Link, SubmitButton, CheckBoxField, TextField
+from wordlette.forms.field_types import (
+    Link,
+    SubmitButton,
+    SelectField,
+)
 from wordlette.requests import Request
+from wordlette.routes.query_vars import QueryArg
 from wordlette.utils.dependency_injection import inject
 
+logger = logging.getLogger(__name__)
 
-class CreateSettingsFileForm(Form):
-    site_name: str @ TextField(
-        placeholder='ex. "Bob\'s Blog" or "Paul\'s Portfolio"',
-        label="What is your website's name?",
-    )
-    domain_name: str @ TextField(
-        placeholder='ex. "bobsblog.com" or "www.paulsportfolio.com"',
-        label="What is your website's domain name?",
-    )
-    force_https: bool @ CheckBoxField(
-        classes=["label-inline"],
-        checked=True,
-        label="Force HTTPS",
+
+class SelectDatabaseTypeForm(Form, method=Request.Get):
+    database_type: str @ SelectField(
+        label="What type of database do you want to use?",
+        placeholder="Select your database",
+        options={},
     )
 
     buttons = (
         Link("Back", href="/"),
         SubmitButton("Next", type="submit"),
     )
-
-    def validate_type_str_isnt_empty(self, value: str):
-        if not value.strip():
-            raise ValueError("Field cannot be empty")
 
 
 class ConfigureDatabase(SetupRoute, setup_category=SetupCategory.Database):
@@ -41,61 +43,60 @@ class ConfigureDatabase(SetupRoute, setup_category=SetupCategory.Database):
     async def setup_status(
         self,
         config: ConfigManager @ inject = None,
-        settings_filename: str @ AppSetting("settings-filename") = None,
-        working_directory: str @ AppSetting("working-directory") = None,
     ) -> SetupStatus:
-        # if config.find_config_file(settings_filename, working_directory):
-        #     return SetupStatus.Complete
+        if config.get("database", DatabaseSettings, None):
+            logger.info("Database is configured")
+            return SetupStatus.Complete
 
+        logger.info("Database is not configured")
         return SetupStatus.Ready
 
-    async def get_setup_page(self, _: Request.Get):
-        return self._create_page_template(
-            form=CreateSettingsFileForm,
-        )
-
-    async def create_settings_file(
+    async def get_setup_page(
         self,
-        form: CreateSettingsFileForm,
+        _: Request.Get,
+        database_type: str @ QueryArg("database-type", None),
+        next_page: str @ QueryArg("nextstep", None),
+    ):
+        if next_page:
+            return RedirectResponse(next_page)
+
+        form = SelectDatabaseTypeForm
+        db_type_field: SelectField = cast(
+            SelectField, form.__form_fields__["database_type"]
+        )
+        db_type_field.options = {
+            driver.nice_name: driver.driver_name
+            for driver in DatabaseDriver.__drivers__.values()
+        }
+        if driver := DatabaseDriver.__drivers__.get(database_type):
+            form = driver.__settings_form__
+
+        return self._create_template(form=form)
+
+    async def save_database_settings(
+        self,
+        request: Request.Post,
+        database_type: str @ QueryArg("database-type", None),
         config: ConfigManager @ inject,
         settings_filename: str @ AppSetting("settings-filename"),
         working_directory: str @ AppSetting("working-directory"),
     ):
-        path = config.write_config_file(
-            settings_filename,
-            working_directory,
-            {
-                "site": {
-                    "domain": form.domain_name,
-                    "https": form.force_https,
-                    "name": form.site_name,
-                }
-            },
-        )
-        subtitle = "Your Settings Have Been Saved"
-        return Template(
-            "create-settings-file.html",
-            title=f"Wordlette: {subtitle}",
-            heading="Setup",
-            subtitle=subtitle,
-            created_settings_file=True,
-            file_path=path,
-            next_page_url=self.get_next_step_url(),
-        )
+        if not (driver := DatabaseDriver.__drivers__.get(database_type)):
+            return RedirectResponse(self.url())
 
-    async def handle_form_validation_errors(
-        self, validation_errors: FormValidationError
-    ):
-        return self._create_page_template(
-            form=validation_errors.form,
-        )
+        form_data = await request.form()
+        try:
+            form = driver.__settings_form__.create_from_form_data(form_data)
+        except FormValidationError as exc:
+            return self._create_template(form=exc.form)
 
-    def _create_page_template(self, **kwargs):
-        subtitle = "Configure Database"
-        return Template(
-            "create-settings-file.html",
-            title=f"Wordlette: {subtitle}",
-            heading="Setup",
-            subtitle=subtitle,
-            **kwargs,
-        )
+        db_settings = config.config.setdefault("database", {})
+        db_settings |= {
+            "driver": database_type,
+            "settings": form.convert_to_dict(),
+        }
+        config.save_to_config_file(settings_filename, working_directory)
+        return await self.complete()
+
+    def _create_template(self, **kwargs):
+        return Template("setup-page.html", subtitle="Configure Database", **kwargs)
