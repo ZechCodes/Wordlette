fdbea827429606613ee88846e12e3012d3db24b9
ZechCodes <hi@zech.codes>
2023-09-26 17:22:00 -0400

    Load the config at setup

--- wordlette/cms/states/setup/states.py
+++ wordlette/cms/states/setup/states.py
@@ -1,8 +1,13 @@
+from contextlib import suppress
+
 from wordlette.cms.states.setup.route_types import (
     SetupRoute,
     SetupRouteCategoryController,
 )
 from wordlette.cms.themes import ThemeManager
+from wordlette.configs.managers import ConfigManager
+from wordlette.core.app import AppSetting
+from wordlette.core.exceptions import ConfigFileNotFound
 from wordlette.middlewares.router_middleware import RouteManager
 from wordlette.state_machines import State
 from wordlette.utils.dependency_injection import AutoInject, inject
@@ -14,7 +19,13 @@ class Setup(State, AutoInject):
         route_manager: RouteManager @ inject,
         theme_manager: ThemeManager @ inject,
         controller: SetupRouteCategoryController @ inject,
+        config: ConfigManager @ inject,
+        settings_filename: str @ AppSetting("settings-filename"),
+        working_directory: str @ AppSetting("working-directory"),
     ):
+        with suppress(ConfigFileNotFound):
+            config.load_config_file(settings_filename, working_directory)
+
         self._load_route_modules()
         if await controller.get_next_route() == controller.completed_route:
             return self.cycle()
