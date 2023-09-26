344c33ba3cb70a79d77e99ad2d59d3b7d7c55626
ZechCodes <hi@zech.codes>
2023-09-26 17:25:52 -0400

    Add a category to the launch page

--- wordlette/cms/states/setup/routes/launch.py
+++ wordlette/cms/states/setup/routes/launch.py
@@ -1,12 +1,13 @@
 from starlette.responses import RedirectResponse
 
+from wordlette.cms.states.setup.enums import SetupCategory
 from wordlette.cms.states.setup.route_types import SetupRoute
 from wordlette.requests import Request
 from wordlette.state_machines import StateMachine
 from wordlette.utils.dependency_injection import inject
 
 
-class Launch(SetupRoute):
+class Launch(SetupRoute, setup_category=SetupCategory.Page):
     path = "/launch"
 
     async def launch_site(self, _: Request.Get, statemachine: StateMachine @ inject):
