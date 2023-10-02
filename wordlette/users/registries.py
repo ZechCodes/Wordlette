0595e95a19c2053a43955f19d0965f86cb45a210
ZechCodes <hi@zech.codes>
2023-10-02 17:01:37 -0400

    Implement a user registry

---
+++ wordlette/users/registries.py
@@ -0,0 +1,37 @@
+from typing import overload
+
+from wordlette.models import Auto
+from wordlette.orm.statuses import (
+    DatabaseStatus,
+)
+from wordlette.users.accessors import UserAccessor
+from wordlette.users.models import User
+
+
+class UserRegistry:
+    @overload
+    def get(self, user_id: int) -> UserAccessor:
+        ...
+
+    @overload
+    def get(self, user: User) -> UserAccessor:
+        ...
+
+    def get(self, user: int | User) -> UserAccessor:
+        match user:
+            case int():
+                user_id = user
+
+            case User():
+                user_id = user.id
+
+            case _:
+                raise TypeError(f"Expected {int | User}, got {type(user)}")
+
+        return UserAccessor(user_id)
+
+    async def add(self, user: User) -> DatabaseStatus:
+        if isinstance(user.id, Auto):
+            return await User.add(user)
+
+        return await user.sync()
