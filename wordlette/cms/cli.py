f43444add07625526201f03e3c596c38d2a37a20
ZechCodes <hi@zech.codes>
2023-10-02 21:24:26 -0400

    Add an auth security level CLI flag

--- wordlette/cms/cli.py
+++ wordlette/cms/cli.py
@@ -1,9 +1,26 @@
+from wordlette.users.auth_security_levels import AuthSecurityLevel
+
+
 def run():
     import sys
 
     match (*map(str.casefold, sys.argv),):
         case (_, "serve", *args):
-            _start_server(debug="--debug" in args or "-d" in args)
+            settings = {}
+            if "--debug" in args or "-d" in args:
+                settings["debug"] = True
+
+            if "--auth-security-level" in args:
+                index = args.index("--auth-security-level")
+                security_level = getattr(
+                    AuthSecurityLevel, args[index + 1].upper(), None
+                )
+                if security_level is None:
+                    raise RuntimeError("Invalid security level")
+
+                settings["min_auth_security_level"] = security_level
+
+            _start_server(**settings)
 
         case _:
             raise RuntimeError("Invalid command")
