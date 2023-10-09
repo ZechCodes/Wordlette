d48eebba86e9beb36e3813726248a0674a59e9da
ZechCodes <hi@zech.codes>
2023-10-09 18:35:14 -0400

    Implement the auth provide setup page

--- wordlette/cms/states/setup/routes/configure_auth.py
+++ wordlette/cms/states/setup/routes/configure_auth.py
@@ -1,31 +1,26 @@
 from typing import cast
 
-from starlette.responses import RedirectResponse
-
 from wordlette.cms.forms import Form
 from wordlette.cms.states.setup.enums import SetupCategory, SetupStatus
 from wordlette.cms.states.setup.route_types import SetupRoute
 from wordlette.cms.themes import Template
 from wordlette.core.app import AppSetting
 from wordlette.core.configs import ConfigManager
-from wordlette.core.forms import FormValidationError
 from wordlette.core.forms.field_types import (
     Link,
     SubmitButton,
-    SelectField,
+    CheckboxGroupField,
 )
 from wordlette.core.requests import Request
 from wordlette.core.routes.query_vars import QueryArg
-from wordlette.orm.drivers import DatabaseDriver
 from wordlette.users.auth_providers import BaseAuthProvider
 from wordlette.utils.dependency_injection import inject
 
 
-class SelectAuthProviderTypesForm(Form, method=Request.Get):
-    authentication_types: list[str] @ SelectField(
-        label="Which authentication methods would you like to use for login forms?",
+class SelectAuthProviderTypesForm(Form):
+    authentication_types: list[str] @ CheckboxGroupField(
         options={},
-        multiple=True,
+        legend="Which login methods would you like to use?",
     )
 
     buttons = (
@@ -57,13 +52,15 @@ class ConfigureAuthProviders(SetupRoute, setup_category=SetupCategory.Admin):
         database_type: str @ QueryArg("authentication-types", None),
     ):
         form = SelectAuthProviderTypesForm
-        auth_types_field: SelectField = cast(
-            SelectField, form.__form_fields__["authentication_types"]
+        auth_types_field: CheckboxGroupField = cast(
+            CheckboxGroupField, form.__form_fields__["authentication_types"]
         )
-        auth_types_field.options = {
+        auth_types_field.options |= {
             driver.nice_name: driver.__qualname__
             for driver in BaseAuthProvider.__auth_provider_registry__
         }
+
+        auth_types_field.options = dict(sorted(auth_types_field.options.items()))
 
         params = {}
         if self._exception:
@@ -72,26 +69,16 @@ class ConfigureAuthProviders(SetupRoute, setup_category=SetupCategory.Admin):
 
         return self._create_template(form=form, **params)
 
-    async def save_database_settings(
+    async def save_authentication_settings(
         self,
-        request: Request.Post,
-        database_type: str @ QueryArg("database-type", None),
+        form: SelectAuthProviderTypesForm,
         config: ConfigManager @ inject,
         settings_filename: str @ AppSetting("settings-filename"),
         working_directory: str @ AppSetting("working-directory"),
     ):
-        if not (driver := DatabaseDriver.__drivers__.get(database_type)):
-            return RedirectResponse(self.url())
-
-        form_data = await request.form()
-        try:
-            form = driver.__settings_form__.create_from_form_data(form_data)
-        except FormValidationError as exc:
-            return self._create_template(form=exc.form)
-
-        config.config["database"] = {"driver": database_type, **form.convert_to_dict()}
+        config.config["authentication"] = {"methods": form.authentication_types}
         config.save_to_config_file(settings_filename, working_directory)
         return await self.complete()
 
     def _create_template(self, **kwargs):
-        return Template("setup-page.html", subtitle="Configure Database", **kwargs)
+        return Template("setup-page.html", subtitle="Configure Login Methods", **kwargs)
