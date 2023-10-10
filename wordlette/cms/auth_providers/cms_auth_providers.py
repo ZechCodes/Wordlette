4778a3392b3512ab03633474f1a72b15b1ba8882
ZechCodes <hi@zech.codes>
2023-10-10 08:44:40 -0400

    Add create acount & login forms/buttons to auth providers

--- wordlette/cms/auth_providers/cms_auth_providers.py
+++ wordlette/cms/auth_providers/cms_auth_providers.py
@@ -1,20 +1,48 @@
+from typing import Any, TypeVar
+
 from wordlette.cms.forms import Form
+from wordlette.cms.themes.components import Component
 from wordlette.users.auth_providers import BaseAuthProvider
+
+T = TypeVar("T", bound=Any)
 
 
 class BaseCMSAuthProvider(BaseAuthProvider, auto_register=False):
+    create_account_button: Component
+    create_account_form: Form
+    login_button: Component
+    login_form: Form
     setup_form: Form | None
 
     def __init_subclass__(
-        cls, setup_form: Form | None = None, auto_register=True, **kwargs
+        cls,
+        auto_register=True,
+        create_account_button: Component | None = None,
+        create_account_form: Form | None = None,
+        login_button: Component | None = None,
+        login_form: Form | None = None,
+        setup_form: Form | None = None,
+        **kwargs,
     ):
-        cls.setup_form = cls._determine_setup_form(setup_form)
+        super().__init_subclass__(auto_register=auto_register, **kwargs)
 
-        super().__init_subclass__(**kwargs)
+        cls.create_account_button = cls._get_value(
+            "create_account_button", create_account_button
+        )
+        cls.login_button = cls._get_value("login_button", login_button)
+
+        cls.create_account_form = cls._get_value("create_account_form", login_form)
+        cls.login_form = cls._get_value("login_form", login_form)
+        cls.setup_form = cls._get_value("setup_form", setup_form, optional=True)
 
     @classmethod
-    def _determine_setup_form(cls, setup_form: Form | None) -> Form | None:
-        if setup_form is not None:
-            return setup_form
+    def _get_value(
+        cls, name: str, override_value: T | None, optional: bool = False
+    ) -> T | None:
+        if override_value is not None:
+            return override_value
 
-        return getattr(cls, "setup_form", None)
+        if (value := getattr(cls, name, None)) is None and not optional:
+            raise ValueError(f"{cls.__name__} must have an attribute {name!r}")
+
+        return value
